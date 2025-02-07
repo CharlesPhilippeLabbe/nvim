1bc20dff3f96bcfe338080f0d2a487a6cb109bb4
Charles-Philippe <c.p.labbe@me.com>
2025-02-07 13:59:36 -0500

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,8 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', 'gr', function() builtin.lsp_references() end);
+vim.keymap.set('n', '<leader>pgb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>pgc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>pls', builtin.lsp_dynamic_workspace_symbols, {})
+
 local setup = require('telescope').setup{defaults ={file_ignore_patterns = {"node%_modules/.*", "./node%_modules/.*", "pkg/*"}} }
