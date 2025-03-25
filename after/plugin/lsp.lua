a1ec0692f588f6fb94eaf86a33ecca039695f7e4
CP <cp@o-possum.com>
2025-03-24 22:28:56 -0400

    Added shortcut for rename

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,8 @@ lsp_zero.on_attach(function(client, bufnr)
 
   
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gk", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "grr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "grn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
