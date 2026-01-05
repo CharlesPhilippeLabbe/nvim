db79d05addc33e424ff4c6d05a3a331df92027c3
clabb <charles-philippe.labbe@cae.com>
2026-01-05 17:07:47 -0500

    Fixed after update

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,15 +1,14 @@
+local harpoon = require("harpoon")
+harpoon:setup()
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+vim.keymap.set("n", "<leader>a", function() harp:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>",ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) vim.cmd("norm zz") end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) vim.cmd("norm zz") end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) vim.cmd("norm zz") end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) vim.cmd("norm zz") end)
 
-vim.keymap.set("n", "<C-h>",function() ui.nav_file(4) vim.cmd("norm zz") end)
-vim.keymap.set("n", "<C-j>",function() ui.nav_file(1) vim.cmd("norm zz") end)
-vim.keymap.set("n", "<C-k>",function() ui.nav_file(2) vim.cmd("norm zz") end)
-vim.keymap.set("n", "<C-l>",function() ui.nav_file(3) vim.cmd("norm zz") end)
-
-vim.keymap.set("n", "<C-1>",function() ui.nav_file(1) vim.cmd("norm zz") end)
-vim.keymap.set("n", "<C-2>",function() ui.nav_file(2) vim.cmd("norm zz") end)
-vim.keymap.set("n", "<C-3>",function() ui.nav_file(3) vim.cmd("norm zz") end)
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
